10a69343a86bcad15ec30422b4424ebfbba120d2
Juan Miguel Choy <juanchoy30@gmail.com>
2021-02-01 11:28:10 -0300

    109.useEffect - Precautions

--- src/components/02-useEffect/Message.js
+++ src/components/02-useEffect/Message.js
@@ -1,19 +1,32 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 export const Message = () => {
 
+
+    const [coords, setCoords] = useState({ x:0, y:0 });
+    const { x, y } = coords;
+
+
     useEffect(() => {
+
+        const mouseMove = (e) => {
+            const coords = { x: e.x, y: e.y };
+            setCoords( coords );
+        }
         
-        console.log('Component loaded');
+        window.addEventListener( 'mousemove', mouseMove );
 
         return () => {
-            console.log('Component unloaded');
+            window.removeEventListener( 'mousemove', mouseMove );
         }
     }, [])
 
     return (
         <div>
             <h3>You are greate!</h3>
+            <p>
+                x:{ x } y:{ y }
+            </p>
         </div>
     
 )
