11d062245c8b2b3be2bf6d63932c48a9ab098f19
Juan Miguel Choy <juanchoy30@gmail.com>
2021-02-01 17:40:53 -0300

    114.UseRef - Real case scenario

--- src/index.js
+++ src/index.js
@@ -7,10 +7,11 @@ import ReactDOM from 'react-dom';
 // import { SimpleForm } from './components/02-useEffect/SimpleForm';
 // import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
 // import { MultipleCustomHooks } from './components/03-Examples/MultipleCustomHooks';
-import { FocusScreen } from './components/04-useRef/FocusScreen';
+// import { FocusScreen } from './components/04-useRef/FocusScreen';
+import { RealExampleRef } from './components/04-useRef/RealExampleRef';
 
 
 ReactDOM.render(
-    <FocusScreen />,
+    <RealExampleRef />,
   document.getElementById('root')
 );
