1dab59f304d56cc779a988ab2567cb43ec917eaa
Juan Miguel Choy <juanchoy30@gmail.com>
2021-02-01 09:06:37 -0300

    Counter with 2 variable useState, setcounter

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HooksApp } from './HooksApp';
+import { CounterApp } from './components/01-useState/CounterApp';
+//import { HooksApp } from './HooksApp';
 
 ReactDOM.render(
-    <HooksApp />,
+    <CounterApp />,
   document.getElementById('root')
 );
