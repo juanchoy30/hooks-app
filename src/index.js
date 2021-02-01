5fe01c4cb7657b3be5eeb104bf4b1bd26a51d66c
Juan Miguel Choy <juanchoy30@gmail.com>
2021-02-01 16:18:21 -0300

    111.useFetch - CustomHooks

--- src/index.js
+++ src/index.js
@@ -5,10 +5,11 @@ import ReactDOM from 'react-dom';
 // import { HooksApp } from './HooksApp';
 // import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
 // import { SimpleForm } from './components/02-useEffect/SimpleForm';
-import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
+// import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
+import { MultipleCustomHooks } from './components/03-Examples/MultipleCustomHooks';
 
 
 ReactDOM.render(
-    <FormWithCustomHook />,
+    <MultipleCustomHooks />,
   document.getElementById('root')
 );
