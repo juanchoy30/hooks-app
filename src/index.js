c95d5f5d933dfe0e3de9e37e0d81e37c134af628
Juan Miguel Choy <juanchoy30@gmail.com>
2021-02-01 09:40:34 -0300

    106.useCounter - CustomHook

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { CounterApp } from './components/01-useState/CounterApp';
+
+import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
+//import { CounterApp } from './components/01-useState/CounterApp';
 //import { HooksApp } from './HooksApp';
 
 ReactDOM.render(
-    <CounterApp />,
+    <CounterWithCustomHook />,
   document.getElementById('root')
 );
