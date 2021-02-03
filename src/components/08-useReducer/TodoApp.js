d0d7844ef0224aa537ad9523817da4ec58a48943
Juan Miguel Choy <juanchoy30@gmail.com>
2021-02-03 11:53:39 -0300

    126.Shell of todo list

--- src/components/08-useReducer/TodoApp.js
+++ src/components/08-useReducer/TodoApp.js
@@ -10,7 +10,7 @@ import './styles.css';
 
 const initialState = [{
     id: new Date().getTime(),
-    desc: 'Learn piano',
+    desc: 'Learn React',
     done: false
 }];
 
@@ -22,14 +22,57 @@ export const TodoApp = () => {
 
     return (
         <div>
-            <h1>Todo App</h1>
+            <h1>Todo App <small>( { todos.length } )</small></h1>
             <hr />
 
-            <ul>
-                <li>Hola</li>
-                <li>Mundo</li>
-                <li>Hola de nuevo</li>
-            </ul>
+            <div className="row">
+
+                <div className="col-7">
+
+                    <ul className="list-group list-group-flush">
+                        {
+                            todos.map( (todo, i) => (
+                                <li
+                                    key={ todo.id }
+                                    className="list-group-item"
+                                    > 
+                                    <p className="text-center"> { i + 1 }. { todo.desc } </p>
+                                    <button
+                                        className="btn btn-danger"
+                                        >
+                                        Delete
+                                    </button>
+                                </li>
+                            ))
+                        }
+                    </ul>
+
+                </div>
+
+                <div className="col-5">
+
+                    <h4>Add todo</h4>
+                    <hr />
+
+                    <form>
+                        <input
+                            type="text"
+                            name="description"
+                            className="form-control"
+                            placeholder="Learn..."
+                            autoComplete="off"
+                        />
+
+                        <button
+                            className="btn btn-outline-primary mt-1 btn-block"
+                        >
+                            Add
+                        </button>
+                    </form>
+
+                </div>
+
+            </div>
 
 
         </div>
