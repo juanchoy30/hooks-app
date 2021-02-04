3b4f00b9ffd33f21ee92c034bae237520ed022b0
Juan Miguel Choy <juanchoy30@gmail.com>
2021-02-04 14:49:45 -0300

    138.Config Router in React

---
+++ src/components/09-useContext/AppRouter.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route
+  } from "react-router-dom";
+
+import { AboutScreen } from './AboutScreen';
+import { HomeScreen } from './HomeScreen';
+import { LoginScreen } from './LoginScreen';
+
+export const AppRouter = () => {
+    return (
+        <Router>
+            <div>
+
+                <Switch>
+                    {/* exact (Exact route) is important if the most general path is included at the begining */}
+                    <Route exact path="/" component={ HomeScreen} />  
+                    <Route exact path="/about" component={ AboutScreen} />
+                    <Route exact path="/login" component={ LoginScreen} />
+
+                </Switch>
+            </div>
+        </Router>
+    )
+}
--- src/components/09-useContext/MainApp.js
+++ src/components/09-useContext/MainApp.js
@@ -1,10 +1,8 @@
-import React from 'react'
+import React from 'react';
+import { AppRouter } from './AppRouter';
 
 export const MainApp = () => {
     return (
-        <div>
-            <h1>Main App</h1>
-            <hr />
-        </div>
+        <AppRouter />
     )
 }
