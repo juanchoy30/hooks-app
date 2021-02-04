27b5ec2fc8e91c4ab66ff3e4053accb84fd3b1ca
Juan Miguel Choy <juanchoy30@gmail.com>
2021-02-04 15:37:50 -0300

    140.CreateContext and useContext

--- src/components/09-useContext/MainApp.js
+++ src/components/09-useContext/MainApp.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import { AppRouter } from './AppRouter';
+import { UserContext } from './UserContext';
 
 export const MainApp = () => {
+
+    const user = {
+        id: 1234,
+        name: 'Juan',
+        email: 'juanchoy30@gmail.com'
+    }
+
     return (
-        <AppRouter />
+        <UserContext.Provider value={ user }>
+
+            <AppRouter />
+
+        </UserContext.Provider>
     )
 }
